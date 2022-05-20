32eebd1cf86b254a9ceffebb111a620e22e9873b
Luana Lessa <lessalsn@gmail.com>
2022-05-20 19:04:00 -0300

    Code review

--- Routers/signUpRouter.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { postSignUp } from '../controllers/signUpController.js';
-
-const signUpRouter = express.Router();
-signUpRouter.post('/sign-up', postSignUp);
-
-export default signUpRouter;
